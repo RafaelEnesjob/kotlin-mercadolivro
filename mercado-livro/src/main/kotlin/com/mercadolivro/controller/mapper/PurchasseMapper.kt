6ebcf8a1cc209b65bb961cf3290f19525e204789
Rafael Enes <rafaeleneswork@gmail.com>
2022-04-27 21:19:08 -0300

    implementando eventos

--- mercado-livro/src/main/kotlin/com/mercadolivro/controller/mapper/PurchasseMapper.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/controller/mapper/PurchasseMapper.kt
@@ -18,7 +18,7 @@ class PurchasseMapper(
 
         return PurchaseModel(
             customer = customer,
-            books = books,
+            books = books.toMutableList(),
             price = books.sumOf { it.price },
         )
     }
