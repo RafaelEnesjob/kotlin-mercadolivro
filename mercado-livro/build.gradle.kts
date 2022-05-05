e0963c11a24e20c8751c8d4992d1598823bf9e62
Rafael Enes <rafaeleneswork@gmail.com>
2022-05-05 20:43:55 -0300

    adicionando spring security criptografando senha

--- mercado-livro/build.gradle.kts
+++ mercado-livro/build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-security")
 
 	implementation("io.springfox:springfox-swagger2:2.9.2")
 	implementation("io.springfox:springfox-swagger-ui:2.9.2")
