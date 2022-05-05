651c99e0d507ed9ebeed7abdd029d51b14fe8bde
Rafael Enes <rafaeleneswork@gmail.com>
2022-05-05 20:12:23 -0300

    criando tabela de roles

--- mercado-livro/src/main/kotlin/com/mercadolivro/controller/request/PostCustomerRequest.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/controller/request/PostCustomerRequest.kt
@@ -9,6 +9,7 @@ data class PostCustomerRequest(
 
     @field:NotEmpty(message = "Nome deve ser informado")
     var name: String,
+
     @field:Email(message = "E-mail deve ser válido")
     @EmailAvaliable
     var email: String,
